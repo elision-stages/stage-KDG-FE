5274672ec91b1e03662b986aba327e2522ffaf75
Anton Vroemans <anton.vroemans@student.kdg.be>
2022-04-21 13:03:03 +0200

    First commit

---
+++ marketplace/src/app/app.main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app.main',
+  templateUrl: './app.main.component.html',
+  styleUrls: ['./app.main.component.scss']
+})
+export class App.MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
