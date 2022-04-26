5530f23e5ba14997536e67ce9aa00874b8ce4586
Anton Vroemans <anton.vroemans@student.kdg.be>
2022-04-26 16:08:34 +0200

    Added basic registration form, still needs interaction

--- marketplace/src/app/components/login/login.component.ts
+++ marketplace/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators } from "@angular/forms";
+import {Router} from "@angular/router";
+import getMailHint from "../../helpers/getMailHint";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public getMailHint = getMailHint;
 
-  constructor() { }
+  loginForm = new FormGroup({
+    mail: new FormControl('', Validators.email),
+    password: new FormControl(''),
+  });
+  registerForm = new FormGroup({
+    mail: new FormControl('', Validators.email),
+  });
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    // ngOnInit is required
   }
 
+  onLogin(): void {
+    console.log(this.loginForm)
+  }
+
+  onRegister(): void {
+    console.log(this.registerForm)
+    sessionStorage.setItem('registerMail', this.registerForm.get('mail').value)
+    this.router.navigate(['/register'])
+  }
 }
