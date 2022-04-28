bac6f80c7ef35f7bf828a3e9d46065d0b28ea934
Anton Vroemans <anton.vroemans@student.kdg.be>
2022-04-28 09:57:45 +0200

    Finalized login UI, added basic auth service, added some favicons

--- marketplace/src/app/components/login/login.component.ts
+++ marketplace/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, Validators } from "@angular/forms";
 import {Router} from "@angular/router";
 import getMailHint from "../../helpers/getMailHint";
 import { MessageService } from 'primeng/api';
+import {AuthService} from "../../service/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -13,23 +14,33 @@ import { MessageService } from 'primeng/api';
 export class LoginComponent implements OnInit {
   public getMailHint = getMailHint;
 
+  loading: boolean = false
   loginForm = new FormGroup({
     mail: new FormControl('', Validators.email),
     password: new FormControl(''),
-  });
+  })
+
   registerForm = new FormGroup({
     mail: new FormControl('', Validators.email),
-  });
+  })
 
-  constructor(private router: Router, private messageService: MessageService) { }
+  constructor(private router: Router, private messageService: MessageService, private authService: AuthService) { }
 
   ngOnInit(): void {
     // ngOnInit is required
   }
 
   onLogin(): void {
-    this.messageService.add({severity:'error', summary: 'Error', detail: 'Unknown error'});
-    console.log(this.loginForm)
+    this.loading = true
+    this.authService.login(this.loginForm.get('username').value, this.loginForm.get('password').value).subscribe({
+      next: (result) => {
+        console.log('Login result', result)
+        this.messageService.add({severity:'error', summary: 'Error', detail: 'This service isn\'t implemented yet'});
+      },
+      complete: () => {
+        this.loading = false
+      }
+    })
   }
 
   onRegister(): void {
