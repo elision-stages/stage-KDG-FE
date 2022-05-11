d2d1a6c609aa9394de69becd806ae88cf60f8747
Anton Vroemans <anton.vroemans@student.kdg.be>
2022-05-12 01:29:01 +0200

    Bugfixes and code smells

--- marketplace/src/app/components/open/product/product.component.ts
+++ marketplace/src/app/components/open/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import staticRandomInt from "../../../helpers/staticRandomInt";
 import {ProductService} from "../../../service/product.service";
