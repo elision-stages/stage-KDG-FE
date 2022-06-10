6e71983e2f09690af4b2e8ec4cae92c04d41cdc7
vanbabe <104006124+vanbabe@users.noreply.github.com>
2022-06-10 18:29:21 +0200

    fixing bugs after be code refactor

--- marketplace/src/app/model/Product.ts
+++ marketplace/src/app/model/Product.ts
@@ -19,3 +19,26 @@ export class Product {
 
 }
 
+export class ProductDto {
+  id: number
+  title: string
+  price: number
+  categoryId: number
+  description: string
+  images: string[]
+  attributes: Array<AttributeValue>
+  vendorId: number
+
+
+  constructor(id: number, title: string, price: number, categoryId: number, description: string, images: string[], attributes: Array<AttributeValue>, vendorId: number) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.categoryId = categoryId;
+    this.description = description;
+    this.images = images;
+    this.attributes = attributes;
+    this.vendorId = vendorId;
+  }
+}
+
